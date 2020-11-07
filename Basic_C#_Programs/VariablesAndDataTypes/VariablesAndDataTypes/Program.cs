39e9486891bf5591e70135c2bf714cea668ee63f
Albert Lee <45klee@gmail.com>
2020-11-06 19:29:23 -0800

    VariablesAndDataTypes

---
+++ Basic_C#_Programs/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+	class Program
+	{
+	static void Main(string[] args)
+			{
+			//// This is a simple program
+			//Console.WriteLine("What is your name?");
+			//string yourName = Console.ReadLine();
+			//Console.WriteLine("Your name is: " + yourName);
+			//Console.ReadLine();
+
+			Console.WriteLine("The Tech Academy");
+			Console.ReadLine();
+
+			Console.WriteLine("Student Daily Report");
+			Console.ReadLine();
+
+			//Console.WriteLine("What is your favorite number?");
+			//string favoriteNumber = Console.ReadLine();
+			//int favNum = Convert.ToInt32(favoriteNumber);
+			//int total = favNum + 5;
+			//Console.WriteLine("Your favorite number plus 5 is: " + total);
+			//Console.ReadLine();
+
+
+			Console.WriteLine("What is your name?");
+			string name = Console.ReadLine();
+			Console.WriteLine("Your name is: " + name);
+			Console.ReadLine();
+
+			Console.WriteLine("What course are you on?");
+			string course = Console.ReadLine();
+			Console.WriteLine("You are on course: " + course);
+			Console.ReadLine();
+
+			Console.WriteLine("What page number?");
+			string pageNumber = Console.ReadLine();
+			int pageNum = Convert.ToInt32(pageNumber);
+			Console.WriteLine("You are on page: " + pageNum);
+			Console.ReadLine();
+
+			Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
+			string help = Console.ReadLine();
+			Console.WriteLine(help);
+			Console.ReadLine();
+
+			Console.WriteLine("Were there any positive experiences you’d like to share ? Please give specifics.");
+			string experience = Console.ReadLine();
+			Console.WriteLine(experience);
+			Console.ReadLine();
+
+			Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+			string feedback = Console.ReadLine();
+			Console.WriteLine(feedback);
+			Console.ReadLine();
+
+			Console.WriteLine("How many hours did you study today?");
+			string hours = Console.ReadLine();
+			int hoursNum = Convert.ToInt32(hours);
+			Console.WriteLine("You are on page: " + hoursNum);
+			Console.ReadLine();
+
+			Console.WriteLine("\"Thank you for your answers. An Instructor will respond to this shortly. Have a great day!\" This is the end of the program.");
+			Console.ReadLine();
+
+			//bool isStudying = false;
+			//byte hoursWorked = 42;
+			//sbyte currentTemperature = -23;
+			//char questionMark = '\u2103';
+
+
+			//decimal moneyInBank = 100.5m;
+
+			//double heightInCentimeters = 211.30202092;
+
+			//float secondsLeft = 2.62f;
+
+			//short temperatureOnMars = -341;
+
+			//string myName = "Jesse";
+
+			//int currentAge = 30;
+			//string yearsOld = currentAge.ToString();
+
+			//bool isRaining = true;
+			//string rainingStatus = Convert.ToString(isRaining);
+			//Console.WriteLine(rainingStatus);
+			//Console.ReadLine();
+
+
+
+			//Console.WriteLine(questionMark);
+			//Console.ReadLine();
+		}
+	}
+}
