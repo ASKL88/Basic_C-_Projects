3ae90eb8987e6c91d6eb271aa73b084024b1e7b4
Albert Lee <45klee@gmail.com>
2020-11-11 20:31:05 -0800

    BranchingAssignment

--- Basic_C#_Programs/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
+++ Basic_C#_Programs/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -10,31 +10,30 @@ namespace BooleanLogicAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your age?");
-            string age = Console.ReadLine();
-            int ageNum = Convert.ToInt32(age);
-            Console.WriteLine("Your age is: " + ageNum);
-            Console.ReadLine();
+        //    Console.WriteLine("What is your age?");
+        //    string age = Console.ReadLine();
+        //    int ageNum = Convert.ToInt32(age);
+        //    Console.WriteLine("Your age is: " + ageNum);
+        //    Console.ReadLine();
 
-            Console.WriteLine("Have you ever had a DUI? Please answer true or false");
-            string dui = Console.ReadLine();
-            bool duianswer = Convert.ToBoolean(dui);
-            Console.WriteLine(duianswer);
-            Console.ReadLine();
+        //    Console.WriteLine("Have you ever had a DUI? Please answer true or false");
+        //    string dui = Console.ReadLine();
+        //    bool duianswer = Convert.ToBoolean(dui);
+        //    Console.WriteLine(duianswer);
+        //    Console.ReadLine();
 
-            Console.WriteLine("How many speeding tickets do you have?");
-            string tick = Console.ReadLine();
-            int tickNum = Convert.ToInt32(tick);
-            Console.WriteLine("Your number of speeding tickets is: " + tickNum);
-            Console.ReadLine();
+        //    Console.WriteLine("How many speeding tickets do you have?");
+        //    string tick = Console.ReadLine();
+        //    int tickNum = Convert.ToInt32(tick);
+        //    Console.WriteLine("Your number of speeding tickets is: " + tickNum);
+        //    Console.ReadLine();
 
-            Console.WriteLine("Qualifed?");
-            string qual = Console.ReadLine();
+        //    Console.WriteLine("Qualifed?");
+        //    string qual = Console.ReadLine();
 
-            bool result = (ageNum > 15 && duianswer == false && tickNum <= 3);
-            Console.WriteLine(result);
-            Console.ReadLine();
-
+        //    bool result = (ageNum > 15 && duianswer == false && tickNum <= 3);
+        //    Console.WriteLine(result);
+        //    Console.ReadLine();
 
         }
     }
---
+++ Basic_C#_Programs/Branching/Branching/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whats your fav num");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string results = favNum == 12 ? "You have an awesome fav num" : "You do not have an awesome fav num";
+
+            Console.WriteLine(results);
+            Console.ReadLine();
+
+
+
+
+
+        //    int roomTemperature = 70;
+
+        //    Console.WriteLine("Hi, what is your name?");
+        //    string name = Console.ReadLine();
+
+        //    Console.WriteLine("Hi " + name + ", what is the temperature where you are?");
+        //    int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+        //    if (currentTemperature == roomTemperature)
+        //    {
+        //        Console.WriteLine("It is exactly room temp");
+        //    }
+        //    else if (currentTemperature > roomTemperature)
+        //    {
+        //        Console.WriteLine("It is warmer than room temp");
+        //    }
+        //    else if (roomTemperature > currentTemperature)
+        //    {
+        //        Console.WriteLine("It is colder than room temp");
+        //    }
+        //    else 
+        //    {
+        //        Console.WriteLine("Something went wrong");
+        //    }
+
+        //    Console.Read();
+            //int currentTemperature = 80;
+            //int roomTemperature = 70;
+
+            //string comparisonResults = currentTemperature == roomTemperature ? "It is room remp" : "It is not room temp";
+
+            //Console.WriteLine(comparisonResults);
+            //Console.ReadLine();
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temp");
+            //}
+
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("Room temp is warmer than current temp");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature");\
+            
+        }
+    }
+}
