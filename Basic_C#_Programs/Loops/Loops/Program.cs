3144ebb9426761698c7dc0081e2f6ea54e2d980b
Albert Lee <45klee@gmail.com>
2020-11-12 17:37:13 -0800

    Loops

---
+++ Basic_C#_Programs/Loops/Loops/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Guess a number?");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Boolean isGuessed = number == 12;
+
+            //do
+            //{
+            //    switch (number)
+            //    {
+            //        case 62:
+            //            Console.WriteLine("You guessed 62. Try again");
+            //            Console.WriteLine("Guess a number?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 29:
+            //            Console.WriteLine("You guessed 29. Try again");
+            //            Console.WriteLine("Guess a number?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 55:
+            //            Console.WriteLine("You guessed 55. Try again");
+            //            Console.WriteLine("Guess a number?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 12:
+            //            Console.WriteLine("You guessed the number 12. That is correct!");
+            //            isGuessed = true;
+            //            break;
+            //        default:
+            //            Console.WriteLine("You are wrong.");
+            //            Console.WriteLine("Guess a number?");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //    }
+            //}
+            //while (!isGuessed);
+
+            //    Console.Read();
+
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            int x = 0;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+            while (x < 5);
+        }
+        }
+    }
