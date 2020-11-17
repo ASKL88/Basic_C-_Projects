0736998a8ff46b8635bedac7ab1bb55cd97e7838
Albert Lee <45klee@gmail.com>
2020-11-16 17:58:23 -0800

    ExceptionAssignment

--- Basic_C#_Programs/ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ Basic_C#_Programs/ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -1,130 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleAppAssignment
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+//namespace ConsoleAppAssignment
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
-            //PART ONE
-            //List<string> Cars = new List<string>() { "Mazda.", "Toyota.", "Ford." };
+//            //PART ONE
+//            List<string> Cars = new List<string>() { "Mazda.", "Toyota.", "Ford." };
 
-            //Console.WriteLine("Please write some text.");
+//            Console.WriteLine("Please write some text.");
 
-            //string text = Console.ReadLine();
+//            string text = Console.ReadLine();
 
-            //foreach (string car in Cars)
-            //{
-            //    Console.WriteLine(car + " " + text);
-            //    Console.ReadLine();
-            //}
-
-
-
-            //PART TWO
-
-            //int count = 0;
-
-            //while (true)
-            //{
-            //    Console.WriteLine("Count = 0.", count);
-            //    count++;
-
-            //    if (count >10)
-            //    {
-            //        break;
-            //    }
-            //}
+//            foreach (string car in Cars)
+//            {
+//                Console.WriteLine(car + " " + text);
+//                Console.ReadLine();
+//            }
 
 
+            
+//            //PART TWO
 
-            //PART THREE
+//            int count = 0;
 
-            //int i = 0;
-            //while (i < 5)
-            //{
-            //    Console.WriteLine(i);
-            //    i++;
-            //}
-            //Console.ReadLine();
+//            while (true)
+//            {
+//                Console.WriteLine("Count = 0.", count);
+//                count++;
 
-
-            //int x = 0;
-            //while (x <= 5)
-            //{
-            //    Console.WriteLine(x);
-            //    x++;
-            //}
-            //Console.ReadLine();
-
-            //PART FOUR
-
-            //List<string> Fruits = new List<string>() { "Banana", "Apple", "Berry", "Grape", "Orange" };
-
-            //Console.WriteLine("Please search for a fruit");
-            //string answer = Console.ReadLine();
-
-            //for (int i = 0; i < Fruits.Count; i++)
-            //{
-            //    if (answer == Fruits[i])
-            //    {
-            //        Console.WriteLine("Match found at: " + i);
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Match not found");
-            //    }
-            //}
-            //Console.ReadLine();
-
-            //PART FIVE
-
-            //List<string> Cars = new List<string>() { "Tesla", "Ford", "Chevy", "Toyota", "Tesla" };
-
-            //Console.WriteLine("Please search for a car");
-            //string carAnswer = Console.ReadLine();
+//                if (count > 10)
+//                {
+//                    break;
+//                }
+//            }
 
 
-            //for (int x = 0; x < Cars.Count; x++)
-            //{
-            //    if (carAnswer == Cars[x])
-            //    {
-            //        Console.WriteLine("Match found at: " + x);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Match not found");
 
-            //    }
-            //}
-            //Console.ReadLine();
+//            //PART THREE
 
-            //PART SIX
+//            int i = 0;
+//            while (i < 5)
+//            {
+//                Console.WriteLine(i);
+//                i++;
+//            }
+//            Console.ReadLine();
 
-            List<string> Vegetables = new List<string>() { "Broccoli", "Celery", "Carrot", "Pea", "Carrot" };
 
-            List<string> VegTemp = new List<string>();
+//            int x = 0;
+//            while (x <= 5)
+//            {
+//                Console.WriteLine(x);
+//                x++;
+//            }
+//            Console.ReadLine();
 
-            foreach (string veggies in Vegetables)
-            {
-                foreach (string temp in VegTemp)
-                {
+//            //PART FOUR
 
-                    if (veggies == temp)
-                    {
-                        Console.WriteLine(veggies + " has appeared before in the list.");
-                    }
+//            List<string> Fruits = new List<string>() { "Banana", "Apple", "Berry", "Grape", "Orange" };
 
-                }
-                VegTemp.Add(veggies);
-            }
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.WriteLine("Please search for a fruit");
+//            string answer = Console.ReadLine();
+
+//            for (int i = 0; i < Fruits.Count; i++)
+//            {
+//                if (answer == Fruits[i])
+//                {
+//                    Console.WriteLine("Match found at: " + i);
+//                    break;
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Match not found");
+//                }
+//            }
+//            Console.ReadLine();
+
+//            //PART FIVE
+
+//            List<string> Cars = new List<string>() { "Tesla", "Ford", "Chevy", "Toyota", "Tesla" };
+
+//            Console.WriteLine("Please search for a car");
+//            string carAnswer = Console.ReadLine();
+
+
+//            for (int x = 0; x < Cars.Count; x++)
+//            {
+//                if (carAnswer == Cars[x])
+//                {
+//                    Console.WriteLine("Match found at: " + x);
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Match not found");
+
+//                }
+//            }
+//            Console.ReadLine();
+
+//            //PART SIX
+
+//            List<string> Vegetables = new List<string>() { "Broccoli", "Celery", "Carrot", "Pea", "Carrot" };
+
+//            List<string> VegTemp = new List<string>();
+
+//            foreach (string veggies in Vegetables)
+//            {
+//                foreach (string temp in VegTemp)
+//                {
+
+//                    if (veggies == temp)
+//                    {
+//                        Console.WriteLine(veggies + " has appeared before in the list.");
+//                    }
+
+//                }
+//                VegTemp.Add(veggies);
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//}
--- Basic_C#_Programs/ExceptionAssignment/ExceptionAssignment/Program.cs
+++ Basic_C#_Programs/ExceptionAssignment/ExceptionAssignment/Program.cs
@@ -10,6 +10,34 @@ namespace ExceptionAssignment
     {
         static void Main(string[] args)
         {
+
+            try
+            {
+                List<int> numbers = new List<int>() { 99, 85, 14, 75, 66, 34 };
+
+                Console.WriteLine("Pick a number to divide by.");
+                int divNumber = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int input in numbers)
+                {
+                    Console.WriteLine(input / divNumber);
+                }
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            finally
+            {
+                Console.WriteLine("Continuing...");
+                Console.ReadLine();
+            }
+
         }
     }
 }
