65d5e9763b561a3a2e6373d25f55ad24004a9990
Albert Lee <45klee@gmail.com>
2020-11-16 20:43:47 -0800

    ClassesAndMethodsAssignment

---
+++ Basic_C#_Programs/ClassesAndMethodsAssignment/ClassesAndMethodsAssignment/Integer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment
+{
+    class Integer
+    {
+        public int First(int x)
+        {
+            return x + 3;   
+        }
+
+        public int Second(int y)
+        {
+            return y * 5;
+        }
+
+        public int Third(int z)
+        {
+            return z / 1;
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassesAndMethodsAssignment/ClassesAndMethodsAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Choose a number to do operations");
+            string answer = Console.ReadLine();
+            int intAnswer = Convert.ToInt32(answer);
+
+            Integer a = new Integer();
+            int i = a.First(intAnswer);
+            int j = a.Second(intAnswer);
+            int k = a.Third(intAnswer);
+
+            Console.WriteLine(i);
+
+            Console.WriteLine(j);
+
+            Console.WriteLine(k);
+            Console.ReadLine();
+
+                
+        }
+    }
+}
