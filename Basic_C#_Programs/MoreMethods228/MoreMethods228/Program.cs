af8e4c5e8b035d32e80f1743f412f93016d391e1
Albert Lee <45klee@gmail.com>
2020-11-18 17:13:29 -0800

    MoreMethods228

---
+++ Basic_C#_Programs/MoreMethods228/MoreMethods228/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods228
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoIntegers a = new TwoIntegers();
+
+            Console.WriteLine("Choose first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose second number");
+            int num2 = Convert.ToInt32(Console.ReadLine()); 
+
+            a.Math(num1, num2);
+            Console.ReadLine();
+
+            a.Math(x:2, y:3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MoreMethods228/MoreMethods228/TwoIntegers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods228
+{
+    class TwoIntegers
+    {
+        public void Math(int x, int y)
+        {
+            int results = x + 5;
+            Console.WriteLine(results);
+            Console.WriteLine(y);
+        }
+
+
+    }
+}
