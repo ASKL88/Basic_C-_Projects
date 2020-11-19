b809fec830827156012793c9267f087d7ad26ba9
Albert Lee <45klee@gmail.com>
2020-11-18 20:15:28 -0800

    ClassMethods231

---
+++ Basic_C#_Programs/ClassMethods231/ClassMethods231/Integer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods231
+{
+    class Integer
+    {
+        public void Math(int x)
+        {
+            int results = x / 2;
+            Console.WriteLine(results);
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassMethods231/ClassMethods231/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integer a = new Integer();
+
+            Console.WriteLine("Choose a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            a.Math(num1);
+            Console.ReadLine();
+        }
+    }
+}
