23c8650ea7a6ad634f14da284328f81f77dcf85d
Albert Lee <45klee@gmail.com>
2020-11-23 17:41:28 -0800

    OperatorOverload252

--- Basic_C#_Programs/PolymorphismAbstract244/PolymorphismAbstract244/Employee.cs
+++ Basic_C#_Programs/PolymorphismAbstract244/PolymorphismAbstract244/Employee.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace PolymorphismAbstract244
 {
-    public class Employee : Person, IQuittable
+    public class Employee: Person, IQuittable
     {
+        public int Id { get; set; }
+
         public override void SayName()
         {
             Console.WriteLine(firstName + " " + lastName);
@@ -18,5 +20,34 @@ namespace PolymorphismAbstract244
             Console.WriteLine("I Quit");
         }
 
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            bool result = false;
+
+            if (employee1.Id == employee2.Id)
+            {
+                result= true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool result = true;
+
+            if (employee1.Id != employee2.Id)
+            {
+                result= true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
     }
 }
--- Basic_C#_Programs/PolymorphismAbstract244/PolymorphismAbstract244/Program.cs
+++ Basic_C#_Programs/PolymorphismAbstract244/PolymorphismAbstract244/Program.cs
@@ -6,20 +6,34 @@ using System.Threading.Tasks;
 
 namespace PolymorphismAbstract244
 {
-     class Program
+    class Program
     {
-         static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Employee employee = new Employee();
+            Employee employee1 = new Employee();
 
-            employee.firstName = "Sample";
-            employee.lastName = "Student";
-            employee.SayName();
-            
+            employee1.firstName = "Sample";
+            employee1.lastName = "Student";
+            employee1.Id = 1;
+            employee1.SayName();
+
+            Employee employee2 = new Employee();
+
+            employee2.firstName = "Sample2";
+            employee2.lastName = "Student2";
+            employee2.Id = 2;
+            employee2.SayName();
+
+            Console.WriteLine(employee1 != employee2);
 
             Employee quit = new Employee();
             quit.Quit();
             Console.ReadLine();
+
+
         }
+
+
+
     }
-}
+    }
