a88859d80e79adf099a263425bda2aaa1d8911de
Albert Lee <45klee@gmail.com>
2020-11-24 17:09:49 -0800

    Structs267

---
+++ Basic_C#_Programs/Structs267/Structs267/Number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structs267
+{
+    public struct Number
+    {
+        public decimal Amount;
+    }
+}
---
+++ Basic_C#_Programs/Structs267/Structs267/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Structs267
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 30.14m;
+
+            Console.WriteLine(number.Amount);
+        }
+    }
+}
