92aa0cd8240b1e317532af22292cbda560ece32c
Albert Lee <45klee@gmail.com>
2020-11-24 16:43:08 -0800

    Enums261

---
+++ Basic_C#_Programs/Enums261/Enums261/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums261
+{
+    class Program
+    {
+        enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current day of the week");
+            string Answer = Console.ReadLine();
+
+            DaysOfTheWeek day;
+            try
+            {
+                day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Answer);
+
+                Console.WriteLine(day);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -10,19 +10,12 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Game game = new TwentyOneGame();
-            game.Players = new List<Player>();
-            Player player = new Player();
-            player.Name = "Jesse";
-            game += player;
-            game -= player;
-
             Deck deck = new Deck();
             deck.Shuffle(3);
 
             foreach (Card card in deck.Cards)
             {
-                Console.WriteLine(card.Face + " " + card.Suit);
+                Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
