014fd413ed77ffd2cf55f79dd62e9b2ab74ff33d
Albert Lee <45klee@gmail.com>
2020-11-26 20:08:42 -0800

    Exceptions314

---
+++ Basic_C#_Programs/Exceptions314/Exceptions314/NoNegative.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions314
+{
+    public class NoNegative : Exception
+    {
+        public NoNegative()
+            : base() { }
+        public NoNegative(string message) 
+            : base(message) { } 
+    }
+
+
+}
---
+++ Basic_C#_Programs/Exceptions314/Exceptions314/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exceptions314
+{
+    class Program
+    {
+        static void CheckFor(int number)
+        {
+            if (number < 1)
+            {
+                throw new NoNegative("Zero or Negative number is not allowed");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What is your age?");
+                int answer = Convert.ToInt32(Console.ReadLine());
+                CheckFor(answer);
+                var year = DateTime.Now.Year;
+                int numYear = Convert.ToInt32(Convert.ToString(year));
+                Console.WriteLine("Birth Year: " + (numYear - answer));
+                Console.ReadLine();
+            }
+            catch (NoNegative)
+            {
+                Console.WriteLine("You must enter a number greater than zero");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You made an error.");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
